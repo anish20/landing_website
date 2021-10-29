464521db4fa1eb5c737809a301e49cc190cb7496
Mohd Anish <anish15.786@gmail.com>
2021-10-29 17:02:09 +0530

    My first comit of landing page website

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(()=>{
+    /*
+    =================Navbar Shrink=========================
+    */
+
+    $(window).on("scroll",()=>{
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+
+    /*
+    =================Video Popup=========================
+    */
+    const videoSrc=$("#player-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click",()=>{
+        if($(".video-popup").hasClass("open")){
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src","");
+        }else{
+            $(".video-popup").addClass("open");
+            if( $("#player-1").attr("src")==''){
+                $("#player-1").attr("src",videoSrc);
+            }
+        }
+    });
+/*========================== Features Carousel============================= */
+$('.features-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1, 
+        },
+        600:{
+            items:2,
+           
+        },
+        1000:{
+            items:3,
+           
+        }
+    }
+})
+
+})
